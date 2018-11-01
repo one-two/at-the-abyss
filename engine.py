6339099614c95cda368bc4a5c9e870b005dbb561
Marcelo Bosso <m.bosso@ymail.com>
2018-11-01 14:57:11 -0300

    day two
    default start

---
+++ engine.py
@@ -0,0 +1,57 @@
+import libtcodpy as libtcod
+from input_handlers import handle_keys
+from entity import Entity
+from render_functions import render_all, clear_all
+from map_objects.game_map import GameMap
+
+def main():
+    screen_width = 80
+    screen_height = 50
+    map_width = 80
+    map_height = 45
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 150),
+        'dark_ground': libtcod.Color(0, 0, 0)
+    }
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.azure)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), 'S', libtcod.yellow)
+    entities = [npc, player]
+
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    libtcod.console_init_root(screen_width, screen_height, 'not new window != 1', False)
+    con = libtcod.console_new(screen_width, screen_height)
+    game_map = GameMap(map_width, map_height)
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        libtcod.console_flush()
+
+        clear_all(con, entities)
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)   
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+
+if __name__ == '__main__':
+    main()
